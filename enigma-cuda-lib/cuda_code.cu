6a7be88fa205fc6901ead1e72b5f691986fd6fc8
matszpk <matszpk@interia.pl>
2017-07-11 15:13:23 +0200

    Fix for windows for CUDA version (fixing unresolved symbols).

--- enigma-cuda-lib/cuda_code.cu
+++ enigma-cuda-lib/cuda_code.cu
@@ -20,13 +20,17 @@
 #include "iterator.h"
 
 
-/*__constant__ int8_t d_ciphertext[MAX_MESSAGE_LENGTH];
+#ifdef _WIN32
+__constant__ int8_t d_ciphertext[MAX_MESSAGE_LENGTH];
 __constant__ Wiring d_wiring;
-__constant__ Key d_key;*/
-//__constant__ NGRAM_DATA_TYPE d_unigrams[ALPSIZE];
+__constant__ Key d_key;
+__constant__ NGRAM_DATA_TYPE d_unigrams[ALPSIZE];
+#endif
 __constant__ NGRAM_DATA_TYPE d_bigrams[ALPSIZE][ALPSIZE];
-/*__constant__ int8_t d_order[ALPSIZE];
-__constant__ int8_t d_plugs[ALPSIZE];*/
+#ifdef _WIN32
+__constant__ int8_t d_order[ALPSIZE];
+__constant__ int8_t d_plugs[ALPSIZE];
+#endif
 __constant__ bool d_fixed[ALPSIZE];
 Result * d_temp;
 
